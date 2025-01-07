b66f095066f90e848ba6a9d4633d199bea378859
Raquel Ramos Backes <raquelrbackes@gmail.com>
2025-01-07 09:28:58 -0300

    feat: created controller to delete store

--- src/modules/stores/controllers/store.controller.ts
+++ src/modules/stores/controllers/store.controller.ts
@@ -1,21 +1,10 @@
-import { Body, Controller, Get, Param, Post, Put, Query, Request } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, Query, Request } from "@nestjs/common";
 import { StoreDto } from "../dto/store.dto";
 import { StoreService } from "../services/store.service";
 
 @Controller('api/v1/store')
 export class StoreController {
   constructor(private readonly storeService: StoreService) {}
-
-  @Post()
-  async createStore(@Body() createStoreDto: StoreDto, @Request() req) {
-    const store = await this.storeService.createStore(createStoreDto, req);
-
-    return {
-      statusCode: 201,
-      message: 'Store created successfully',
-      data: store
-    }
-  }
 
   @Get()
   async getAllStores(@Query('limit') limit: number = 10, @Query('offset') offset: number = 0, @Request() req, ) {
@@ -33,6 +22,28 @@ export class StoreController {
     }
   }
 
+  @Get(':id')
+  async getStoreById(@Param('id') id: string, @Request() req ) {
+    const store = await this.storeService.getStoreById(id, req);
+
+    return {
+      statusCode: 200,
+      message: 'Store fetched successfully',
+      data: store
+    }
+  }
+
+  @Post()
+  async createStore(@Body() createStoreDto: StoreDto, @Request() req) {
+    const store = await this.storeService.createStore(createStoreDto, req);
+
+    return {
+      statusCode: 201,
+      message: 'Store created successfully',
+      data: store
+    }
+  }
+
   @Put(':id')
   async updateStore(@Param('id') id: string, @Body() storeDto: StoreDto, @Request() req) {
     const updatedStore = await this.storeService.updateStore(id, storeDto, req);
@@ -44,15 +55,15 @@ export class StoreController {
     }
   }
 
-  @Get(':id')
-  async getStoreById(@Param('id') id: string, @Request() req ) {
-    const store = await this.storeService.getStoreById(id, req);
+  @Delete(':id')
+  async deleteStore(@Param('id') id: string, @Request() req) {
+    await this.storeService.deleteStore(id, req);
 
     return {
       statusCode: 200,
-      message: 'Store fetched successfully',
-      data: store
+      message: 'Store deleted successfully'
     }
   }
+
 
 }
